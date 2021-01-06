dc91f358a05813b2de45a9849221d20f7b0849a7
Atiacht <atiacht@gmail.com>
2021-01-06 10:59:29 +0300

    #2

--- src/main/Java/web/MainApp.java
+++ src/main/Java/web/MainApp.java
@@ -1,41 +1,41 @@
-package web;
-
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import web.config.AppConfig;
-import web.model.User;
-import web.service.UserService;
-
-public class MainApp {
-    public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        UserService userService = context.getBean(UserService.class);
-
+//package web;
+//
+//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//import web.config.AppConfig;
+//import web.model.User;
+//import web.service.UserService;
+//
+//public class MainApp {
+//    public static void main(String[] args) {
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+//        UserService userService = context.getBean(UserService.class);
+//
+////        userService.deleteAll();
+//
+//        User user1 = new User("Jimmy", 27);
+//        User user2 = new User("Johnny", 57);
+//        User user3 = new User("Janis", 27);
+//
+//        userService.addUser(user1);
+//        userService.addUser(user2);
+//        userService.addUser(user3);
+//        userService.addUser("Kyle", 37);
+//
+//        userService.allUsers().forEach(System.out::println);
+//
+//        System.out.println("\n User id 1 - " + userService.show(1));
+//        System.out.println("Update user - 1");
+//        userService.updateUser(1, "Nikifor", 99);
+//        System.out.println("User id 1 - " + userService.show(1));
+//
+//        System.out.println("\nDelete user - 1");
+//        userService.removeUser(1);
+//        System.out.println("User id 1 - " + userService.show(1));
+//
 //        userService.deleteAll();
-
-        User user1 = new User("Jimmy", 27);
-        User user2 = new User("Johnny", 57);
-        User user3 = new User("Janis", 27);
-
-        userService.addUser(user1);
-        userService.addUser(user2);
-        userService.addUser(user3);
-        userService.addUser("Kyle", 37);
-
-        userService.allUsers().forEach(System.out::println);
-
-        System.out.println("\n User id 1 - " + userService.show(1));
-        System.out.println("Update user - 1");
-        userService.updateUser(1, "Nikifor", 99);
-        System.out.println("User id 1 - " + userService.show(1));
-
-        System.out.println("\nDelete user - 1");
-        userService.removeUser(1);
-        System.out.println("User id 1 - " + userService.show(1));
-
-        userService.deleteAll();
-        System.out.println("List users after deleting - ");
-        userService.allUsers().forEach(System.out::println);
-
-
-    }
-} // #1
+//        System.out.println("List users after deleting - ");
+//        userService.allUsers().forEach(System.out::println);
+//
+//
+//    }
+//} // #2
---
+++ src/main/Java/web/controller/UserController.java
@@ -0,0 +1,55 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
+import web.service.UserService;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("")
+    public String getAllUsers(Model model) {
+        model.addAttribute("users", userService.allUsers()); // for forEach
+        model.addAttribute("newUser", new User()); // for POST
+        return "users";
+    }
+
+    @PostMapping("")
+    public String createNewUser(@ModelAttribute User user, Model model) {
+        userService.addUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/{id}")
+    public String getOneUser(@PathVariable("id") int id, Model model ) {
+        User user = userService.show(id);
+        model.addAttribute("thisUser", user);
+        return "user";
+    }
+
+    @PatchMapping("/{id}")
+    public String editUser(@PathVariable("id") int id, @ModelAttribute User user, Model model) {
+        userService.updateUser(id, user.getName(), user.getAge());
+        String url = String.format("redirect:/users/%d", user.getId());
+        return url;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@ModelAttribute User user, @PathVariable("id") int id, Model model) {
+        userService.removeUser(id);
+        return "redirect:/users";
+
+    }
+
+}
